05aeb445a6818acfb7c3136252ed741077626cf2
arpi-odoo <arpi@odoo.com>
2026-01-23 13:17:17 +0100

    Add Eruda

---
+++ eruda/__manifest__.py
@@ -0,0 +1,17 @@
+
+{
+    'name': 'Eruda Mobile Debug',
+    'category': 'Development',
+    'sequence': 999,
+    'summary': 'Injects Eruda console for mobile debugging',
+    'depends': [
+        'web',
+    ],
+    'data': [
+        'views/templates.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'author': 'Arthur Pierrot',
+    'license': 'LGPL-3',
+}

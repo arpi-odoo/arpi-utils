2e43727690aff8df1ea98afcb75a7c6bb5c25b06
arpi-odoo <arpi@odoo.com>
2026-08-25 15:11:21 +0200

    Fix website team tab not showing to public

--- eva/controllers/main.py
+++ eva/controllers/main.py
@@ -7,7 +7,7 @@ class EvaWebsite(http.Controller):
     @http.route('/team', type='http', auth='public', website=True, sitemap=True)
     def team_page(self, **kwargs):
         teams = request.env['eva.team'].sudo().search([('short_name', '=', 'TS')], order='name')
-        players = request.env['eva.player']
+        players = request.env['eva.player'].sudo()
         for team in teams:
             players |= team.captain_id + (team.player_ids - team.captain_id)
         captain_ids = set(teams.captain_id.ids)
